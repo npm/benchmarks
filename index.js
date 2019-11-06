cf7ba3d9c19b1f0c224e80d5a47ef05c53ad47df
Michael Perrotte <mike@npmjs.com>
2019-11-06 13:05:56 -0500

    feat: updated index to accempt command name; updated logging

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 'use strict'
 
+const comment = require('./lib/comment')
 const execute = require('./lib/execute')
 const { log } = require('./lib/utils')
 
-// INFO: can potentially pass in command line arguments
 const args = process.argv.slice(2)
-const latest = (args.length && args[0] === 'latest')
+const command = args.length && args[0]
+const isRelease = args.length && args[1]
 
 log.verbose('ARGS:', args)
-if (latest) {
-  log.info('Executing benchmark against latest release')
-} else {
-  log.info('Executing benchmark against a version')
+const { PR_ID, REPO, OWNER } = process.env
+
+switch (command) {
+  case 'benchmark':
+    log.info('Executing benchmark against latest release')
+    execute(!!isRelease)
+    break
+  case 'comment':
+    // TODO: bail out if we don't have correct environment variables
+    log.info(`Posting Comment to ${OWNER}/${REPO}/pulls/${PR_ID}`)
+    comment()
+    break
+  default:
+    log.error('Invalid argument supplied...')
+    log.error('Please use the npm-scripts.')
 }
-execute(latest)
